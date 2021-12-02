612ff098a9a60c3466de143db26f966ffc2147d3
Vale2197 <valentinatrebbi97@gmail.com>
2021-12-02 12:08:18 +0100

    prova casuale snack_1

---
+++ snack_1/index.php
@@ -0,0 +1,57 @@
+<?php
+/* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 */
+
+$array = [
+    [
+        "data"  => "sabato 8" [
+            "sCasa" => "Olimpia Milano",
+            "sOspite" => "Cantu",
+            "punteggio" => "55-60",
+        ],
+        
+    ],
+
+    [
+         "data"  => "sabato 9" [
+             "sCasa" => "Olimpia Milano",
+             "sOspite" => "squadra",
+             "punteggio" => "22-30",
+         ],
+    ],
+
+    [
+        
+        "data"  => "sabato 10" [
+             "sCasa" => "Olimpia Milano",
+             "sOspite" => "Cantu",
+             "punteggio" => "30-40",
+         ]
+
+    ]
+    
+];
+
+var_dump($array[0]);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partite</title>
+</head>
+<body>
+    
+<h1>
+    squadre
+</h1>
+
+<p>
+
+</p>
+</body>
+</html>
