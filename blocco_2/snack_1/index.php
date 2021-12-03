a7ac7a4bbf7fc454a82ddae019efbdacd82de20d
Vale2197 <valentinatrebbi97@gmail.com>
2021-12-03 12:33:10 +0100

    completato snack_1 con immagine/link

--- blocco_2/snack_1/index.php
+++ blocco_2/snack_1/index.php
@@ -51,3 +51,20 @@ for ($i=0; $i < count($trueAds); $i++) {
 var_dump($ad);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <a href=" <?php echo $ad['link'] ?> ">
+        <img src=" <?php echo $ad['image_path'] ?> " alt="#">
+    </a>   
+
+
+</body>
+</html>
