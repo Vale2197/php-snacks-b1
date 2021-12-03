36deee5fe6946b39fd63db274c27b5f572835d7d
Vale2197 <valentinatrebbi97@gmail.com>
2021-12-03 12:48:44 +0100

    completato snack_2

--- blocco_2/snack_2/indexInvitati.php
+++ blocco_2/snack_2/indexInvitati.php
@@ -7,5 +7,26 @@ $invitati = [
     'Angela',
 ];
 
-var_dump(ucwords($_GET['name']));
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack_2/2</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            if(in_array(ucwords($_GET['name']), $invitati)) {
+                echo 'ok';
+            }
+            else {
+                echo 'ko';
+            }
+        ?>
+    </h1>
+</body>
+</html>
